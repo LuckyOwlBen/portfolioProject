0a4e1f1ec8fb02ab4b8bed9f083e2769f8b9e6e4
Ben <benmalmberg2@gmail.com>
2024-02-06 12:30:30 -0800

    Added first landing page and route

--- portfolioProject/src/app/app.routes.ts
+++ portfolioProject/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingViewComponent } from './views/landingView/landing-view/landing-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: LandingViewComponent },
+];
---
+++ portfolioProject/src/app/views/landingView/landing-view/landing-view.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SchedulerComponent } from '../../../components/scheduler/scheduler/scheduler.component';}
+
+@Component({
+  selector: 'app-landing-view',
+  standalone: true,
+  imports: [
+    SchedulerComponent,
+  ],
+  templateUrl: './landing-view.component.html',
+  styleUrl: './landing-view.component.scss'
+})
+export class LandingViewComponent {
+
+}

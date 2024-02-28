5bf38a8f12f90989e08854ff3c61a98c717a6f1c
Ben <benmalmberg2@gmail.com>
2024-02-28 13:12:51 -0800

    configured schedule form to block time slots

--- portfolioProject/src/app/components/scheduler/scheduler.component.ts
+++ portfolioProject/src/app/components/scheduler/scheduler.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CalendarEntry } from '../../http_models/responses/availability-response';
-import {MatExpansionModule} from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card'
 
 @Component({
   selector: 'app-scheduler',
   standalone: true,
   providers: [],
   imports: [
-    MatExpansionModule,
     MatButtonModule,
+    MatCardModule,
   ],
   templateUrl: './scheduler.component.html',
   styleUrl: './scheduler.component.scss'

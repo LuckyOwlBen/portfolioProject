78e65ca86003fca304622ba6fd2a5c4a84873748
Ben <benmalmberg2@gmail.com>
2024-02-29 12:02:34 -0800

    ensure back end calendar data stays in sync on the front end

--- portfolioProject/src/app/components/calendar-component/calendar/calendar.component.ts
+++ portfolioProject/src/app/components/calendar-component/calendar/calendar.component.ts
@@ -1,11 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
-import { CalendarEntry } from '../../../http_models/responses/availability-response';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { format } from 'date-fns';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -27,9 +25,8 @@ export class CalendarComponent {
 
   selectedDate: Date = new Date();
   @Output()
-  availabilityEmitter: EventEmitter<CalendarEntry | null> = new EventEmitter();
-  @Input({required: true})
-  calendarData: Map<String, CalendarEntry> = new Map<String, CalendarEntry>();
+  dateEmitter: EventEmitter<Date | null> = new EventEmitter();
+
   calendarForm: FormGroup = this.formBuilder.group({
     dateInput: ['', Validators.required],
     slotInput: ['', Validators.required],
@@ -37,25 +34,12 @@ export class CalendarComponent {
   constructor(private formBuilder: FormBuilder,){}
 
   selectDate($event: MatDatepickerInputEvent<Date>) {
-    const selectedDate = $event.value;
-    const calendarEntry = selectedDate
-      ? this.retrieveOrGenerateCalendarEntry(selectedDate)
-      : null;
-      this.emitCurrentAvailibilityMap(calendarEntry);
+      this.emitSelectedDate($event.value);
   }
 
-  retrieveOrGenerateCalendarEntry(selectedDate: Date): CalendarEntry {
-    let calendarEntry = this.calendarData.get(format(new Date(selectedDate), "yyyy-MM-dd"));
-    if(calendarEntry == undefined) {
-      calendarEntry = {
-        date: format(new Date(selectedDate), "yyyy-MM-dd"),
-        availabilityMap: new Map().set(1, 0).set(2, 0).set(3,0).set(4,0),
-      } as CalendarEntry
-    }
-    return calendarEntry;
-  }
+  
 
-  emitCurrentAvailibilityMap(calendarEntry: CalendarEntry | null | undefined): void {
-    this.availabilityEmitter.emit(calendarEntry);
+  emitSelectedDate(selectedDate: Date | null | undefined): void {
+    this.dateEmitter.emit(selectedDate);
   }
 }
--- portfolioProject/src/app/views/schedule-appointment-view/schedule-appointment-view.component.ts
+++ portfolioProject/src/app/views/schedule-appointment-view/schedule-appointment-view.component.ts
@@ -3,7 +3,7 @@ import { AppointmentService, AvailabilityService } from '../../api-services/api-
 import { AvailabilityRequest } from '../../http_models/requests/availability-request';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { AvailabilityResponse, CalendarEntry } from '../../http_models/responses/availability-response';
-import { addDays, format } from 'date-fns';
+import { addDays, compareAsc, compareDesc, format } from 'date-fns';
 import { CalendarComponent } from '../../components/calendar-component/calendar/calendar.component';
 import { SchedulerComponent } from '../../components/scheduler/scheduler.component';
 import { NgIf } from '@angular/common';
@@ -29,8 +29,11 @@ export class ScheduleAppointmentViewComponent implements OnInit {
 
   readonly store = inject(AppStore);
   availabilityMap: Map<String, CalendarEntry> = new Map<String, CalendarEntry>();
-  @Input()
   calendarEntry!: CalendarEntry | null;
+  firstAvailableDate: Date = addDays(new Date(), 1);
+  lastDateQueried: Date = addDays(new Date(), 30);
+  selectedDate: Date | null = null;
+
   constructor(
     private availabilityService: AvailabilityService,
     private appointmentService: AppointmentService,
@@ -38,24 +41,7 @@ export class ScheduleAppointmentViewComponent implements OnInit {
     private route: ActivatedRoute,
   ) { };
   ngOnInit(): void {
-    this.availabilityService
-      .callApi(null, this.generateAvailabilityRequest(format(new Date(), "yyyy/MM/d"), format(addDays(new Date(), 30), "yyyy/MM/d")))
-      .pipe()
-      .subscribe({
-        next: (availabilityResponse: AvailabilityResponse) => {
-          availabilityResponse.calendarEntries.forEach(entry => {
-            entry.availabilityMap = new Map<number, number>(
-              Object.entries(entry.availabilityMap).map(([k, v]) => [+k, +v])
-            )
-            this.availabilityMap.set(entry.date, entry);
-          })
-        },
-        error: (error: any) => {
-          this.router.navigate(['/error'])
-          console.log(error);
-        },
-      }
-    );
+    this.callAvailabilityService(this.firstAvailableDate, this.lastDateQueried);
   }
 
   generateAvailabilityRequest(date1: String, date2: String): AvailabilityRequest {
@@ -65,8 +51,10 @@ export class ScheduleAppointmentViewComponent implements OnInit {
     } as AvailabilityRequest;
   }
 
-  populateSelectedAvailabilityMap(selectedDayAvailability: CalendarEntry | null) {
-    this.calendarEntry = selectedDayAvailability;
+  populateSelectedDate(selectedDayAvailability: Date | null): void {
+    selectedDayAvailability
+      ? this.processSelectedDate(selectedDayAvailability)
+      : null;
   }
 
   bookAppointment($event: ScheduleAppointmentRequest) {
@@ -82,6 +70,53 @@ export class ScheduleAppointmentViewComponent implements OnInit {
         error: () =>{
           this.router.navigate(['/error'], { relativeTo: this.route })
         }
+      }
+    );
+  }
+
+  retrieveOrGenerateCalendarEntry(selectedDate: Date): void {
+    let calendarEntry = this.availabilityMap.get(format(new Date(selectedDate), "yyyy-MM-dd"));
+    if(calendarEntry == undefined) {
+      calendarEntry = {
+        date: format(new Date(selectedDate), "yyyy-MM-dd"),
+        availabilityMap: new Map().set(1, 0).set(2, 0).set(3,0).set(4,0),
+      } as CalendarEntry
+    }
+    this.calendarEntry = calendarEntry;
+  }
+
+  mapResponseToAvailabilityMap(availabilityResponse: AvailabilityResponse): void {
+    availabilityResponse.calendarEntries.forEach(entry => {
+      entry.availabilityMap = new Map<number, number>(
+        Object.entries(entry.availabilityMap).map(([k, v]) => [+k, +v])
+      )
+      this.availabilityMap.set(entry.date, entry);
+    })
+  }
+
+  processSelectedDate(date: Date): void {
+    this.selectedDate = date;
+    compareDesc(this.lastDateQueried, date) == 1
+      ? this.callAvailabilityService(this.lastDateQueried, date)
+      : this.retrieveOrGenerateCalendarEntry(date);
+  }
+
+  callAvailabilityService(firstDate: Date, secondDate: Date): void {
+    this.availabilityService
+      .callApi(null, this.generateAvailabilityRequest(format(firstDate, "yyyy/MM/d"), format(secondDate, "yyyy/MM/d")))
+      .pipe()
+      .subscribe({
+        next: (availabilityResponse: AvailabilityResponse) => {
+          this.mapResponseToAvailabilityMap(availabilityResponse);
+          this.lastDateQueried = secondDate;
+          if(this.selectedDate !== null) {
+            this.retrieveOrGenerateCalendarEntry(this.selectedDate);
+          }
+        },
+        error: (error: any) => {
+          this.router.navigate(['/error'])
+          console.log(error);
+        },
       }
     );
   }
